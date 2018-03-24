0ffc2bc5e1aae845616a2ba9063a3b8b78a91505
MaciejWikira <maciej.wiqira@gmail.com>
2018-03-24 18:42:10 +0100

    Course ep.36 complete

--- src/main/java/wikira/springdidemo/services/GreetingServicePrimaryIcelandic.java
+++ src/main/java/wikira/springdidemo/services/GreetingServicePrimaryIcelandic.java
@@ -1,16 +1,16 @@
 package wikira.springdidemo.services;
 
+
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
 @Primary
-@Profile({"en", "default"})
-public class GreetingServicePrimary implements GreetingService {
-
+@Profile("is")
+public class GreetingServicePrimaryIcelandic implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "A wizard is never late nor too early, but comes exactly when he chooses to.";
+        return "Halló vinur.";
     }
 }
